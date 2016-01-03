cf8ca16d5fb045c3c18d449967fbf79c0e4cbf78
Michael Erasmus <michaelerasmus@gmail.com>
2016-01-02 20:49:13 -0800

    Added 2 json udfs, one that will get the key name in an object using an index and another that gives you the value at a key(same as json_extract_path_text, but returns a null instead of empty string for missing keys)

--- lib/udf_json_objects.rb
+++ lib/udf_json_objects.rb
@@ -24,6 +24,62 @@ class UdfJsonObjects
                            {query: "select ?(null)", expect: nil},
                            {query: "select ?('abc')", expect: nil},
                        ]
+      },
+      {
+          type:        :function,
+          name:        :json_object_key_at_index,
+          description: "Returns the string value of the key in a json object at a specified index, or null if the index is out of range",
+          params:      "j varchar(max), i int",
+          return_type: "varchar(max)",
+          body:        %~
+            import json
+            if not j:
+              return None
+            try:
+              dict = json.loads(j)
+            except ValueError:
+              return None
+            if len(dict.keys()) - 1 < i:
+                return None
+            return dict.keys()[i]
+          ~,
+          tests:       [
+                           {query: "select ?('{\"foo\" : \"bar\"}',0)", expect: "foo", example: true},
+                           {query: "select ?('{\"foo\" : \"bar\", \"baz\" : 1}' ,1)", expect: "baz", example: true},
+                           {query: "select ?('{\"foo\" : \"bar\"}', 1)", expect: nil, example: true},
+                           {query: "select ?('{}', 0)", expect: nil, example: true},
+                           {query: "select ?('',0)", expect: nil},
+                           {query: "select ?(null,0)", expect: nil},
+                           {query: "select ?('abc',0)", expect: nil},
+                       ]
+      },
+      {
+          type:        :function,
+          name:        :json_object_value_at_key,
+          description: "Returns the string value of in a json object at a specified key, or null if the index is out of range",
+          params:      "j varchar(max), k varchar",
+          return_type: "varchar(max)",
+          body:        %~
+            import json
+            if not j:
+              return None
+            try:
+              dict = json.loads(j)
+            except ValueError:
+              return None
+            if len(dict.keys()) - 1 < i:
+                return None
+            return dict.keys()[i]
+          ~,
+          tests:       [
+                           {query: "select ?('{\"foo\" : \"bar\"}',0)", expect: "foo", example: true},
+                           {query: "select ?('{\"foo\" : \"bar\", \"baz\" : 1}' ,1)", expect: "baz", example: true},
+                           {query: "select ?('{\"foo\" : \"bar\"}', 1)", expect: nil, example: true},
+                           {query: "select ?('{}', 0)", expect: nil, example: true},
+                           {query: "select ?('',0)", expect: nil},
+                           {query: "select ?(null,0)", expect: nil},
+                           {query: "select ?('abc',0)", expect: nil},
+                       ]
       }
   ]
 end
