ff8147eae7df980a3d9be4781cacada360b43a95
Michael Erasmus <michaelerasmus@gmail.com>
2016-02-26 12:21:31 -0800

    json_object_bigint_value_at_key function

--- lib/udf_json_objects.rb
+++ lib/udf_json_objects.rb
@@ -45,7 +45,7 @@ class UdfJsonObjects
           ~,
           tests:       [
                            {query: "select ?('{\"foo\" : \"bar\"}',0)", expect: "foo", example: true},
-                           {query: "select ?('{\"foo\" : \"bar\", \"baz\" : 1}' ,1)", expect: "baz", example: true},
+                           {query: "select ?('{\"foo\" : \"bar\", \"baz\" : 1}' ,'baz')", expect: 1, example: true},
                            {query: "select ?('{\"foo\" : \"bar\"}', 1)", expect: nil, example: true},
                            {query: "select ?('{}', 0)", expect: nil, example: true},
                            {query: "select ?('',0)", expect: nil},
@@ -57,7 +57,7 @@ class UdfJsonObjects
           type:        :function,
           name:        :json_object_value_at_key,
           description: "Returns the string value of in a json object at a specified key, or null if the index is out of range",
-          params:      "j varchar(max), i int",
+          params:      "j varchar(max), k varchar(max)",
           return_type: "varchar(max)",
           body:        %~
             import json
@@ -67,18 +67,52 @@ class UdfJsonObjects
               dict = json.loads(j)
             except ValueError:
               return None
-            if len(dict.keys()) - 1 < i:
+            if len(dict.keys()) == 0:
                 return None
-            return dict.keys()[i]
+            return_val = (dict.get(k))
+            return str(return_val) if return_val else None
           ~,
           tests:       [
-                           {query: "select ?('{\"foo\" : \"bar\"}',0)", expect: "foo", example: true},
-                           {query: "select ?('{\"foo\" : \"bar\", \"baz\" : 1}' ,1)", expect: "baz", example: true},
-                           {query: "select ?('{\"foo\" : \"bar\"}', 1)", expect: nil, example: true},
-                           {query: "select ?('{}', 0)", expect: nil, example: true},
-                           {query: "select ?('',0)", expect: nil},
-                           {query: "select ?(null,0)", expect: nil},
-                           {query: "select ?('abc',0)", expect: nil},
+                           {query: "select ?('{\"foo\" : \"bar\"}','foo')", expect: "bar", example: true},
+                           {query: "select ?('{\"foo\" : \"bar\", \"baz\" : 1}' ,'baz')", expect: "1", example: true},
+                           {query: "select ?('{\"foo\" : \"bar\"}', 'baz')", expect: nil, example: true},
+                           {query: "select ?('{}', 'foo')", expect: nil, example: true},
+                           {query: "select ?('','foo')", expect: nil},
+                           {query: "select ?(null,'foo')", expect: nil},
+                           {query: "select ?('abc','foo')", expect: nil},
+                       ]
+      },
+      {
+          type:        :function,
+          name:        :json_object_bigint_value_at_key,
+          description: "Returns the string value of in a json object at a specified key, or null if the index is out of range",
+          params:      "j varchar(max), k varchar(max)",
+          return_type: "bigint",
+          body:        %~
+            import json
+            import re
+            if not j:
+              return None
+            try:
+              dict = json.loads(j)
+            except ValueError:
+              return None
+            if len(dict.keys()) == 0:
+                return None
+            try:
+                return int(dict.get(k))
+            except:
+                return None
+          ~,
+          tests:       [
+                           {query: "select ?('{\"foo\" : \"bar\", \"baz\" : 1}' ,'baz')", expect: 1, example: true},
+                           {query: "select ?('{\"foo\" : \"bar\"}','foo')", expect: nil, example: true},
+                           {query: "select ?('{\"foo\" : \"bar\"}', 'baz')", expect: nil, example: true},
+                           {query: "select ?('{}', 'foo')", expect: nil, example: true},
+                           {query: "select ?('{\"foo\" : \"\"}', 'foo')", expect: nil},
+                           {query: "select ?('','foo')", expect: nil},
+                           {query: "select ?(null,'foo')", expect: nil},
+                           {query: "select ?('abc','foo')", expect: nil},
                        ]
       }
   ]
